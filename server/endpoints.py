70da38a06f4bcd26a9f413442e8879a99513991c
muddin3031 <32686654+muddin3031@users.noreply.github.com>
2022-09-13 13:27:53 -0400

    Initial commit

---
+++ server/endpoints.py
@@ -0,0 +1,56 @@
+"""
+This is the file containing all of the endpoints for our flask app.
+The endpoint called `endpoints` will return all available endpoints.
+"""
+
+from flask import Flask
+from flask_restx import Resource, Api
+# import db.db as db
+
+app = Flask(__name__)
+api = Api(app)
+
+LIST = 'list'
+HELLO = '/hello'
+MESSAGE = 'message'
+
+
+@api.route(HELLO)
+class HelloWorld(Resource):
+    """
+    The purpose of the HelloWorld class is to have a simple test to see if the
+    app is working at all.
+    """
+    def get(self):
+        """
+        A trivial endpoint to see if the server is running.
+        It just answers with "hello world."
+        """
+        return {MESSAGE: 'hello world'}
+
+
+@api.route(f'/character_types/{LIST}')
+class CharacterTypeList(Resource):
+    """
+    This will get a list of character types.
+    """
+    def get(self):
+        """
+        Returns a list of character types.
+        """
+        return {'hello': 'world'}
+
+
+@api.route('/endpoints')
+class Endpoints(Resource):
+    """
+    This class will serve as live, fetchable documentation of what endpoints
+    are available in the system.
+    """
+    def get(self):
+        """
+        The `get()` method will return a list of available endpoints.
+        """
+        endpoints = ''
+        # sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
